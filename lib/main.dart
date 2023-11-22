c178bd6aae0eb01bf7f673b06aa6b5b8dcf7d5d5
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-11-22 19:13:02 -0300

    map

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
+import './respostas.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -14,32 +16,32 @@ class PerguntaAppState extends State<PerguntaApp> {
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
+      },
+      {
+        'texto': 'Qual é o seu instrutor favorito?',
+        'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+      }
     ];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          // ignore: prefer_const_constructors
-          title: Text('Perguntas'),
+          title: const Text('Perguntas'),
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas[_perguntaSelecionada]),
-            ElevatedButton(
-              child: Text('Resposta 1'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 2'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 3'),
-              onPressed: _responder,
-            ),
+            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+            Resposta('Resposta ', _responder),
+            Resposta('Resposta ', _responder),
+            Resposta('Resposta ', _responder),
           ],
         ),
       ),
@@ -48,6 +50,9 @@ class PerguntaAppState extends State<PerguntaApp> {
 }
 
 class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+
+  @override
   PerguntaAppState createState() {
     return PerguntaAppState();
   }
---
+++ lib/respostas.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+  final String texto;
+  final void Function() quandoSelecionado;
+
+  const Resposta(this.texto, this.quandoSelecionado, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        //textColor: Colors.white,
+        style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.blue),        
+        onPressed: quandoSelecionado,
+        child: const Text('texto'),
+      ),
+    );
+  }
+}
